da4942df97fd09451b3fcad0c4b432025a6b1b12
sridhar nalla <sridhar.nalla@hotmail.com>
2025-07-19 16:33:20 +0530

    feat: initialize project structure and add setup configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llmops',
+    version='0.1.0',
+    author='Sridhar Nalla',
+    description='A package for LLM operations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sridhar-nalla/llmops2',
+    packages=find_packages(),
+  
+)
